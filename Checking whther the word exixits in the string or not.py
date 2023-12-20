d6ded05ecd382320f8050e74829bb59030e20242
RAJAT SONAR <150504303+rajatsonar22@users.noreply.github.com>
2023-12-20 11:09:22 +0530

    Add files via upload

---
+++ Checking whther the word exixits in the string or not.py
@@ -0,0 +1,14 @@
+#Checking memebership in operator
+#checking whether the element or word exits in the string
+
+print("""Checking whther the word is present in the string
+    s = FYCS GOOD MORNING GUYS """)
+
+s = "HLO FYCS GOOD MORNING"
+ch = 'y'
+ch in s
+False
+ch = 'Y'
+ch in s
+
+# RUN THS PROGRAM IN A INTERACTIVE MODE FOR RESULTS
